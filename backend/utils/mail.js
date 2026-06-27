c2c6e2a062d8b2b085b1c764dde01501141bb2c1
Biswajit <pattanaikbiswajit07@gmail.com>
2026-06-27 19:59:23 +0530

    Update mail.js

--- backend/utils/mail.js
+++ backend/utils/mail.js
@@ -1,31 +1,24 @@
-import nodemailer from "nodemailer"
+import { Resend } from "resend"
 import dotenv from "dotenv"
 dotenv.config()
-const transporter = nodemailer.createTransport({
-  service: "Gmail",
-  port: 465,
-  secure: true, // true for 465, false for other ports
-  auth: {
-    user: process.env.EMAIL,
-    pass: process.env.PASS,
-  },
-});
 
-export const sendOtpMail=async (to,otp) => {
-    await transporter.sendMail({
-        from:process.env.EMAIL,
+const resend = new Resend(process.env.RESEND_API_KEY)
+const FROM_EMAIL = "Vingo <onboarding@resend.dev>"
+
+export const sendOtpMail = async (to, otp) => {
+    await resend.emails.send({
+        from: FROM_EMAIL,
         to,
-        subject:"Reset Your Password",
-        html:`<p>Your OTP for password reset is <b>${otp}</b>. It expires in 5 minutes.</p>`
+        subject: "Reset Your Password",
+        html: `<p>Your OTP for password reset is <b>${otp}</b>. It expires in 5 minutes.</p>`
     })
 }
 
-
-export const sendDeliveryOtpMail=async (user,otp) => {
-    await transporter.sendMail({
-        from:process.env.EMAIL,
-        to:user.email,
-        subject:"Delivery OTP",
-        html:`<p>Your OTP for delivery is <b>${otp}</b>. It expires in 5 minutes.</p>`
+export const sendDeliveryOtpMail = async (user, otp) => {
+    await resend.emails.send({
+        from: FROM_EMAIL,
+        to: user.email,
+        subject: "Delivery OTP",
+        html: `<p>Your OTP for delivery is <b>${otp}</b>. It expires in 5 minutes.</p>`
     })
 }
